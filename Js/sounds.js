d210db674c2371dd2385bbb4d0a13d6758a91963
Vinicius Raitz Pilati <122880182+ViniciusPilati33333@users.noreply.github.com>
2023-03-27 16:54:22 -0300

    Update sounds.js

--- Js/sounds.js
+++ Js/sounds.js
@@ -1,8 +1,10 @@
+
+
 export function Sound() {
     const buttonPressAudio = new Audio(
       "https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true"
     )
-}
+
     const kitchenTimer = new Audio(
       "https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true"
     );
@@ -27,3 +29,5 @@ export function Sound() {
         timeEnd,
         playForestAudio,
     }
+  }
+  
